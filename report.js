e419d328f9c5371a72c1e3ebcfb993e44e1ec8c8
Tim Schlechter <timschlechter@gmail.com>
2014-05-16 22:18:00 +0200

    First commit

---
+++ report.js
@@ -0,0 +1,382 @@
+var report = {
+  "generator": "SpecFlow.Reporting.WebApp.WebAppReporter",
+  "features": [
+    {
+      "description": "Story\r\n=====\r\nIn order to get testresults presented in a web application\r\nAs a developer\r\nI want to learn about [SpecFlow.Reporting.WebApp](https://www.nuget.org/packages/SpecFlow.Reporting.WebApp/)",
+      "description_html": "<h1>Story</h1><p>In order to get testresults presented in a web application\r<br />As a developer\r<br />I want to learn about <a href=\"https://www.nuget.org/packages/SpecFlow.Reporting.WebApp/\" target=\"_blank\">SpecFlow.Reporting.WebApp</a></p>",
+      "scenarios": [
+        {
+          "given": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I'm on \"http://nuget.org\"",
+                "start_time": "2014-05-16T22:16:54.485+02:00",
+                "end_time": "2014-05-16T22:16:56.164+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\38b0b425-a5a6-49f1-a08d-2288f4eaefc1.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:16:54.441+02:00",
+            "end_time": "2014-05-16T22:16:56.639+02:00"
+          },
+          "when": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I enter searchtext \"SpecFlow.Reporting.WebApp\" in \"searchBoxInput\"",
+                "start_time": "2014-05-16T22:16:56.649+02:00",
+                "end_time": "2014-05-16T22:16:56.792+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\6b607a1c-3f22-4fa5-b040-3ed24c269e5f.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the search button \"searchBoxSubmit\"",
+                "start_time": "2014-05-16T22:16:57.316+02:00",
+                "end_time": "2014-05-16T22:16:58.618+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\2b5daf80-7226-4c03-8170-2f04e5370392.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the result with title \"SpecFlow.Reporting.WebApp\"",
+                "start_time": "2014-05-16T22:16:58.845+02:00",
+                "end_time": "2014-05-16T22:17:00.299+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\5ef61637-cff6-4883-aa74-5d6995d4cc02.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:16:56.639+02:00",
+            "end_time": "2014-05-16T22:17:00.677+02:00"
+          },
+          "then": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I can read the instructions on how to install the package",
+                "start_time": "2014-05-16T22:17:00.68+02:00",
+                "end_time": "2014-05-16T22:17:00.68+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\1ce174a4-bfd5-4a70-a771-8bd0faad4b5c.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:00.677+02:00",
+            "end_time": "2014-05-16T22:17:01.095+02:00"
+          },
+          "result": "OK",
+          "tags": [
+            "Howto:install"
+          ],
+          "title": "Learn how to install the SpecFlow.Reporting.WebApp package",
+          "start_time": "2014-05-16T22:16:54.438+02:00",
+          "end_time": "2014-05-16T22:17:01.097+02:00"
+        }
+      ],
+      "result": "OK",
+      "tags": [
+        "Plugin"
+      ],
+      "title": "Generate a WebApp out of SpecFlow test results",
+      "start_time": "2014-05-16T22:16:54.426+02:00",
+      "end_time": "2014-05-16T22:17:01.106+02:00"
+    },
+    {
+      "description": "Story\r\n=====\r\nIn order to get testresults in JSON format\r\nAs a developer\r\nI want to learn about [SpecFlow.Reporting.Json](https://www.nuget.org/packages/SpecFlow.Reporting.Json/)",
+      "description_html": "<h1>Story</h1><p>In order to get testresults in JSON format\r<br />As a developer\r<br />I want to learn about <a href=\"https://www.nuget.org/packages/SpecFlow.Reporting.Json/\" target=\"_blank\">SpecFlow.Reporting.Json</a></p>",
+      "scenarios": [
+        {
+          "given": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I'm on \"http://nuget.org\"",
+                "start_time": "2014-05-16T22:17:01.108+02:00",
+                "end_time": "2014-05-16T22:17:01.445+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\b42e4e28-0742-4824-994d-bc604cb17a4f.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:01.108+02:00",
+            "end_time": "2014-05-16T22:17:01.936+02:00"
+          },
+          "when": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I enter searchtext \"SpecFlow.Reporting.Json\" in \"searchBoxInput\"",
+                "start_time": "2014-05-16T22:17:01.936+02:00",
+                "end_time": "2014-05-16T22:17:02.026+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\c52660f7-11d0-4c7f-aac4-35c48a8de2fe.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the search button \"searchBoxSubmit\"",
+                "start_time": "2014-05-16T22:17:02.452+02:00",
+                "end_time": "2014-05-16T22:17:03.227+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\6becd232-a82c-44fe-bd85-1b4cb92fc113.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the result with title \"SpecFlow.Reporting.Json\"",
+                "start_time": "2014-05-16T22:17:03.433+02:00",
+                "end_time": "2014-05-16T22:17:04.399+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\efc3eb3a-bcf4-45c7-8317-f46fe2ceb3ee.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:01.936+02:00",
+            "end_time": "2014-05-16T22:17:04.871+02:00"
+          },
+          "then": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I can read the instructions on how to install the package",
+                "start_time": "2014-05-16T22:17:04.871+02:00",
+                "end_time": "2014-05-16T22:17:04.871+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\55bd6180-c268-4074-8bbf-8666afc3668b.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:04.871+02:00",
+            "end_time": "2014-05-16T22:17:05.283+02:00"
+          },
+          "result": "OK",
+          "tags": [
+            "Howto:install"
+          ],
+          "title": "Learn how to install the SpecFlow.Reporting.Json package",
+          "start_time": "2014-05-16T22:17:01.108+02:00",
+          "end_time": "2014-05-16T22:17:05.283+02:00"
+        }
+      ],
+      "result": "OK",
+      "tags": [
+        "Plugin"
+      ],
+      "title": "Json reporting",
+      "start_time": "2014-05-16T22:17:01.107+02:00",
+      "end_time": "2014-05-16T22:17:05.283+02:00"
+    },
+    {
+      "description": "Story\r\n=====\r\nIn order to get testresults in plain text format\r\nAs a developer\r\nI want to learn about [SpecFlow.Reporting.PlainText](https://www.nuget.org/packages/SpecFlow.Reporting.PlainText/)",
+      "description_html": "<h1>Story</h1><p>In order to get testresults in plain text format\r<br />As a developer\r<br />I want to learn about <a href=\"https://www.nuget.org/packages/SpecFlow.Reporting.PlainText/\" target=\"_blank\">SpecFlow.Reporting.PlainText</a></p>",
+      "scenarios": [
+        {
+          "given": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I'm on \"http://nuget.org\"",
+                "start_time": "2014-05-16T22:17:05.284+02:00",
+                "end_time": "2014-05-16T22:17:05.707+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\bc19accd-e867-4fc1-b748-f9ad3f8b41cf.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:05.284+02:00",
+            "end_time": "2014-05-16T22:17:06.15+02:00"
+          },
+          "when": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I enter searchtext \"SpecFlow.Reporting.PlainText\" in \"searchBoxInput\"",
+                "start_time": "2014-05-16T22:17:06.15+02:00",
+                "end_time": "2014-05-16T22:17:06.217+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\0a7c4e1e-c1c7-4d32-b9c5-20aa24f8ba2e.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the search button \"searchBoxSubmit\"",
+                "start_time": "2014-05-16T22:17:06.638+02:00",
+                "end_time": "2014-05-16T22:17:07.381+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\7289facf-c8ee-4f26-9b4b-a5d6fe94a0cb.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the result with title \"SpecFlow.Reporting.PlainText\"",
+                "start_time": "2014-05-16T22:17:07.631+02:00",
+                "end_time": "2014-05-16T22:17:08.482+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\6714ae61-9544-45ed-9363-a13c2ba77c34.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:06.15+02:00",
+            "end_time": "2014-05-16T22:17:08.997+02:00"
+          },
+          "then": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I can read the instructions on how to install the package",
+                "start_time": "2014-05-16T22:17:08.997+02:00",
+                "end_time": "2014-05-16T22:17:08.997+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\6187af64-f02a-4b7e-8649-475b9f373fd2.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:08.997+02:00",
+            "end_time": "2014-05-16T22:17:09.789+02:00"
+          },
+          "result": "OK",
+          "tags": [
+            "Howto:install"
+          ],
+          "title": "Learn how to install the SpecFlow.Reporting.PlainText package",
+          "start_time": "2014-05-16T22:17:05.284+02:00",
+          "end_time": "2014-05-16T22:17:09.789+02:00"
+        }
+      ],
+      "result": "OK",
+      "tags": [
+        "Plugin",
+        "Plugin"
+      ],
+      "title": "Plain Text reporting",
+      "start_time": "2014-05-16T22:17:05.284+02:00",
+      "end_time": "2014-05-16T22:17:09.789+02:00"
+    },
+    {
+      "description": "Story\r\n=====\r\nIn order to get testresults in XML format\r\nAs a developer\r\nI want to learn about [SpecFlow.Reporting.Xml](https://www.nuget.org/packages/SpecFlow.Reporting.Xml/)",
+      "description_html": "<h1>Story</h1><p>In order to get testresults in XML format\r<br />As a developer\r<br />I want to learn about <a href=\"https://www.nuget.org/packages/SpecFlow.Reporting.Xml/\" target=\"_blank\">SpecFlow.Reporting.Xml</a></p>",
+      "scenarios": [
+        {
+          "given": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I'm on \"http://nuget.org\"",
+                "start_time": "2014-05-16T22:17:09.792+02:00",
+                "end_time": "2014-05-16T22:17:10.106+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\750dd957-4835-4055-90f0-1d787a929933.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:09.792+02:00",
+            "end_time": "2014-05-16T22:17:10.492+02:00"
+          },
+          "when": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I enter searchtext \"SpecFlow.Reporting.Xml\" in \"searchBoxInput\"",
+                "start_time": "2014-05-16T22:17:10.493+02:00",
+                "end_time": "2014-05-16T22:17:10.563+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\c3ce1666-a588-4e43-afd9-a894910c84e0.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the search button \"searchBoxSubmit\"",
+                "start_time": "2014-05-16T22:17:10.977+02:00",
+                "end_time": "2014-05-16T22:17:11.855+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\60c8b977-1a4d-4cf3-b0b0-a6acf19d66e2.png"
+                },
+                "result": "OK"
+              },
+              {
+                "steps": [],
+                "title": "I click the result with title \"SpecFlow.Reporting.Xml\"",
+                "start_time": "2014-05-16T22:17:12.208+02:00",
+                "end_time": "2014-05-16T22:17:13.178+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\5a436c89-8676-40ea-8a34-a058e67e4fcc.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:10.492+02:00",
+            "end_time": "2014-05-16T22:17:13.588+02:00"
+          },
+          "then": {
+            "result": "OK",
+            "steps": [
+              {
+                "steps": [],
+                "title": "I can read the instructions on how to install the package",
+                "start_time": "2014-05-16T22:17:13.588+02:00",
+                "end_time": "2014-05-16T22:17:13.588+02:00",
+                "user_data": {
+                  "screenshot": "screenshots\\2e2fa322-4a8d-4907-a7e1-3e9a6511b87a.png"
+                },
+                "result": "OK"
+              }
+            ],
+            "start_time": "2014-05-16T22:17:13.588+02:00",
+            "end_time": "2014-05-16T22:17:13.976+02:00"
+          },
+          "result": "OK",
+          "tags": [
+            "Howto:install"
+          ],
+          "title": "Learn how to install the SpecFlow.Reporting.Xml package",
+          "start_time": "2014-05-16T22:17:09.792+02:00",
+          "end_time": "2014-05-16T22:17:13.977+02:00"
+        }
+      ],
+      "result": "OK",
+      "tags": [
+        "Plugin",
+        "Plugin"
+      ],
+      "title": "XML reporting",
+      "start_time": "2014-05-16T22:17:09.79+02:00",
+      "end_time": "2014-05-16T22:17:13.977+02:00"
+    }
+  ],
+  "result": "OK",
+  "start_time": "2014-05-16T22:16:54.426+02:00",
+  "end_time": "2014-05-16T22:17:15.54+02:00"
+};
